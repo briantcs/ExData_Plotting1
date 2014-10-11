8bce36f11297b7241958bfd0b6e2d6d99dae17aa
briantcs <briantcs88@gmail.com>
2014-10-11 21:47:45 +0800

    new update

--- plot2.R
+++ plot2.R
@@ -1,14 +1,31 @@
+origin_data <- read.table("household_power_consumption.txt", 
+                        sep=";",
+                        header=TRUE,
+                        quote= "",
+                        strip.white=TRUE,
+                        stringsAsFactors = FALSE,
+                        na.strings= "?")
+
+data <- subset(origin_data, (origin_data$Date == "1/2/2007" | origin_data$Date== "2/2/2007")) 
 
 
-require(sqldf)
-data <- read.csv.sql( file='household_power_consumption.txt',
-                      sep=";",
-                      sql="select * from file where Date = '1/2/2007' or Date = '2/2/2007'",
-                      header=TRUE)
-date_time<-as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %T")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$DateTime <- as.POSIXct(paste(data$Date,data$Time))
 
-with(data,plot(date_time,data$Global_active_power,main="",xlab="",ylab="Global Active Power (kilowatts)"))
+# Constructing the plot and saving it to a PNG file with a width of 480 pixels and a height of 480 pixels.
 
-dev.copy(png,file="plot1.png")
+# Opening the PNG graphic device
+png(filename="plot2.png",
+    width=480, height=480,
+    units="px",
+    bg = "transparent")
 
+# creating a plot using base plotting system 
+plot(data$DateTime,
+     data$Global_active_power,
+     type = "l", 
+     lwd=1,
+     xlab = "",  ylab = "Global Active Power (kilowatts)")
+
+# Closing the PNG device
 dev.off()
